2d4837818a3731c62e73600f53f0a6372f283880
AnhDung13 <ad137ocean@gmail.com>
2024-05-12 00:02:43 +0700

    nộp bài tập buổi 21

---
+++ js/index.js
@@ -0,0 +1,107 @@
+// bài 1
+
+var numbers = [15, 0, 5, -1, 8];
+var max = numbers[0];
+var min = numbers[0];
+var maxIndex = 0;
+var minIndex = 0;
+for (var index in numbers) {
+  if (max < numbers[index]) {
+    max = numbers[index];
+    maxIndex = index;
+  }
+}
+for (var index in numbers) {
+  if (min > numbers[index]) {
+    min = numbers[index];
+    minIndex = index;
+  }
+}
+document.write(
+  "<p>Bài 1: phần tử lớn nhất trong mảng <span style='color:blue;'>" +
+    numbers.join(", ") +
+    " </span>là<span style='color:blue;'> " +
+    max +
+    " </span>vị trí index <span style='color:blue;'>" +
+    maxIndex +
+    " </span>phần tử nhỏ nhất: <span style='color:blue;'>" +
+    min +
+    " </span>vị trí <span style='color:blue;'>" +
+    minIndex +
+    "</span></p>"
+);
+
+// bài 2
+
+var numbers = [0, 15, 20, 2, 3, 12, 5, -1, 7, 4];
+var primeNumbers = [];
+
+for (var value of numbers) {
+  for (var i = 2; i <= Math.sqrt(value); i++) {
+    if (value % i === 0) {
+      value = false;
+    }
+  }
+  if (value === false || value <= 0 || value === 1) {
+    continue;
+  }
+  primeNumbers[primeNumbers.length] = value;
+}
+if (primeNumbers.length === 0) {
+  document.write(
+    "<p>Bài 2: Không có số nguyên tố nào trong mảng <span style='color:blue;'>" +
+      numbers.join(", ") +
+      "</span></p>"
+  );
+} else {
+  var sum = 0;
+  for (var value of primeNumbers) {
+    sum += value;
+  }
+  document.write(
+    "<p>Bài 2: số nguyên tố trong mảng <span style='color:blue;'>" +
+      numbers.join(", ") +
+      " </span>là <span style='color:blue;'>" +
+      primeNumbers.join(", ") +
+      "</span> trung bình các số nguyên tố trong mảng: <span style='color:blue;'>" +
+      sum / primeNumbers.length +
+      "</span></p>"
+  );
+}
+
+//bài 3
+var Arr = [1, 3, 5, 1, 2, 6, 5, 7, 7, 9, "dũng", 8, 11, "dũng", "dũng"];
+var newArr = [];
+
+for (var value of Arr) {
+  for (var index in newArr) {
+    if (newArr[index] === value) {
+      value = false;
+    }
+  }
+  if (value === false) {
+    continue;
+  }
+  newArr[newArr.length] = value;
+}
+
+document.write(
+  "<p>Bài 3: mảng <span style='color:blue;'>" +
+    Arr.join(", ") +
+    "</span> sau khi lọc trùng là: <span style='color:blue;'>" +
+    newArr.join(", ") +
+    "</span></p>"
+);
+
+// bài 4
+var numbers = [2, 5, 1, 9, 8, 10];
+var sortNumbers = [];
+for (var index in numbers) {
+  if (numbers[index] > numbers[index + 1]) {
+    var temp = numbers[index];
+    numbers[index] = numbers[index + 1];
+    numbers[index + 1] = temp;
+    sortNumbers[sortNumbers.length] = numbers[index];
+  }
+}
+console.log(sortNumbers);
